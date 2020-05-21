05fbdd9e9e87454da94673f9006610529302c813
Joseba Merino <yosiunico@gmail.com>
2020-05-21 19:02:45 +0200

    Basic fortuneJS structure

--- lib/server.ts
+++ lib/server.ts
@@ -1,1 +1,28 @@
 // Aqui viene el programa principal : )
+import * as http from 'http';
+import * as fortuneHTTP from 'fortune-http';
+
+import { store } from './store';
+import { request, response } from 'express';
+
+const PORT = 3001;
+
+const listener = fortuneHTTP(store, {
+  // The order specifies priority of media type negotiation.
+  serializers: [
+    fortuneHTTP.JsonSerializer,
+    fortuneHTTP.HtmlSerializer,
+    fortuneHTTP.FormDataSerializer,
+    fortuneHTTP.FormUrlEncodedSerializer,
+  ],
+});
+
+const server = http.createServer((request, response) => {
+  listener(request, response)
+  .catch( error => {
+    // Error logging
+  });
+});
+
+server.listen(PORT);
+console.log(`Server running at http://localhost:${PORT} !`);
--- lib/store.ts
+++ lib/store.ts
@@ -1,10 +1,23 @@
 import * as fortune from 'fortune';
-import * as memAdapter from 'fortune-';
 
+
+// Type definition
 const recordTypes = {
   photo: {
     url: String,
   },
 };
+
+// Adapter interface
+/*
+const pgAdapter = require('fortune-postgres')
+
+const adapter = [ pgAdapter, {
+  // In this example, the Postgres adapter requires the connection URL.
+  url: 'postgres://postgres@localhost:5432/app_db'
+} ]
+
+const store = fortune(recordTypes, { adapter })
+*/
 
 export const store = fortune(recordTypes);
